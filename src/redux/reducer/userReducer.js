ed5750ba558a03d68979464725038ccab905d039
hungoke <nvh66725@gmail.com>
2021-03-19 16:53:21 +0700

    commit

--- src/redux/reducer/userReducer.js
+++ src/redux/reducer/userReducer.js
@@ -1,10 +1,13 @@
 import { users } from "../state/users"
+const initialState = {
+  users,
+}
 
-const userReducer = (state = { users }, action) => {
+const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case "AddNewUser":
-      users.push(action.user)
-      return { users: [...users] }
+      // users.push(action.user)
+      return { ...state, users: [...users, action.user] }
 
     case "DeleteUser":
       const indexOfUser = users.findIndex((user) => user.id === action.user.id)
